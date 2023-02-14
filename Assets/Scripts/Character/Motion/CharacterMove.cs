687da7448251372226e6ce584ee4cc78c996ac78
포페 <84568588+re-exe@users.noreply.github.com>
2023-02-14 15:59:58 +0900

    fix:キャラクター同士の接触処理を改善

--- Assets/Scripts/Character/Motion/CharacterMove.cs
+++ Assets/Scripts/Character/Motion/CharacterMove.cs
@@ -13,6 +13,39 @@ public class CharacterMove : MonoBehaviour{
     [Tooltip("ID")]
     private TREE_ID id = TREE_ID.MAX;
 
+    [Tooltip("現在の挙動")]
+    private CHARA_STATE m_state = CHARA_STATE.DUMMY;
+
+    [Tooltip("移動速度")]
+    private float moveSpeed = 0f;
+
+    [Tooltip("攻撃力")]
+    private float attackPower = 0f;
+
+    [SerializeField]
+    [Tooltip("HP")]
+    private float hp = 0f;
+
+    [Tooltip("HP(代入用)")]
+    public float charaHP {get; set;}
+
+    [Tooltip("Rigidbody")]
+    private Rigidbody2D m_rigidbody = null;
+
+    [Tooltip("情報リスト")]
+    private List<CharacterParamAsset.CharacterParam> m_paramList = new List<CharacterParamAsset.CharacterParam>();
+
+    [Tooltip("接触している相手の情報")]
+    private EnemyMove m_enemyMove = null;
+
+    [Tooltip("キャラクターの挙動")]
+    private enum CHARA_STATE{
+        DUMMY,
+        MOVE,
+        ATTACK,
+        WAIT
+    }
+
     [Tooltip("キャラクターの種類ID")]
     public enum TREE_ID{
         SOMEIYOSHINO,
@@ -24,13 +57,86 @@ public class CharacterMove : MonoBehaviour{
     }
 
     private void Awake() {
-        
-        switch(id){
-            case TREE_ID.SOMEIYOSHINO: break;
-            case TREE_ID.KANHIZAKURA:  break;
-            case TREE_ID.KAWAZUSAKURA: break;
-            case TREE_ID.HIGANZAKURA:  break;
-            case TREE_ID.OYAMAZAKURA:  break;
+
+        CharacterParamAsset characterParam = Resources.Load("CharacterParamAsset") as CharacterParamAsset;
+        m_paramList = characterParam.characterParamList;
+
+        // init
+        moveSpeed = m_paramList[(int)id].moveSpeed;
+        attackPower = m_paramList[(int)id].attackPower;
+        charaHP = m_paramList[(int)id].hp;
+        hp = m_paramList[(int)id].hp;
+        m_rigidbody = GetComponent<Rigidbody2D>();
+
+        moveSpeed = moveSpeed / 150f;
+
+        m_state = CHARA_STATE.MOVE;
+
+        CharacterBehavior(id);
+    }
+
+    private void CharacterBehavior(TREE_ID id){
+
+        this.FixedUpdateAsObservable()
+            .Where(_=> m_state != CHARA_STATE.DUMMY)
+            .Subscribe(_=>{
+
+                switch(m_state){
+                    case CHARA_STATE.MOVE:
+
+                        m_rigidbody.AddForce(this.transform.right * -moveSpeed);
+                        break;
+
+                    case CHARA_STATE.ATTACK:
+
+                        m_rigidbody.velocity = Vector2.zero;
+                        
+
+                        break;
+
+                    case CHARA_STATE.WAIT:
+                        m_rigidbody.velocity = Vector2.zero;
+
+                        break;
+                }
+
+                // 自動殺戮処理
+                if(this.transform.position.x < -20f){
+                    Destroy(this.gameObject);
+                }
+
+                if(charaHP <= 0f){
+                    Destroy(this.gameObject);
+                }
+
+                hp = charaHP;
+
+            }).AddTo(this);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Enemy")){
+            m_state = CHARA_STATE.ATTACK;
+
+            m_enemyMove = other.gameObject.GetComponent<EnemyMove>();
+            StartCoroutine(AttackCol(m_paramList[(int)id].frequency));
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Enemy")){
+            m_state = CHARA_STATE.MOVE;
+            StopCoroutine(AttackCol(m_paramList[(int)id].frequency));
+        }
+    }
+
+    private IEnumerator AttackCol(float waittime){
+
+        while(true){
+
+            m_enemyMove.enemyHP = m_enemyMove.enemyHP - attackPower;
+
+            yield return new WaitForSeconds(waittime);
         }
     }
 }
--- Assets/Scripts/Enemy/EnemyMove.cs
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -14,22 +14,104 @@ public class EnemyMove : MonoBehaviour{
     [Header("攻撃力")]
     private float attackPower = 0f;
 
+    [SerializeField]
+    [Header("攻撃スパン")]
+    private float attackSpan = 0f;
+
+    [SerializeField]
+    [Tooltip("HP")]
+    private float hp = 0f;
+
+    [Tooltip("HP(代入用)")]
+    public float enemyHP {get; set;}
+
     [Tooltip("Rigidbody")]
     private Rigidbody2D m_rigidbody = null;
+
+    [Tooltip("現在の挙動")]
+    private ENEMY_STATE m_state = ENEMY_STATE.DUMMY;
+
+    [Tooltip("接触している相手の情報")]
+    private CharacterMove m_charaMove = null;
+
+    [Tooltip("敵の挙動")]
+    private enum ENEMY_STATE{
+        DUMMY,
+        MOVE,
+        ATTACK,
+        WAIT
+    }
 
     private void Awake() {
         m_rigidbody = GetComponent<Rigidbody2D>();
 
+        enemyHP = hp;
+
         moveSpeed = moveSpeed / 150f;
 
-        this.FixedUpdateAsObservable()
-            .Where(_=> m_rigidbody)
-            .Subscribe(_=>{
-                m_rigidbody.AddForce(this.transform.right * moveSpeed);
+        m_state = ENEMY_STATE.MOVE;
+        EnemyBehavior();
 
+    }
+
+    private void EnemyBehavior(){
+
+        this.FixedUpdateAsObservable()
+            .Where(_=> m_state != ENEMY_STATE.DUMMY)
+            .Subscribe(_=>{
+
+                switch(m_state){
+                    case ENEMY_STATE.MOVE:
+
+                        m_rigidbody.AddForce(this.transform.right * moveSpeed);
+                        
+                        break;
+
+                    case ENEMY_STATE.ATTACK:
+
+                        m_rigidbody.velocity = Vector2.zero;
+
+                        break;
+
+                    case ENEMY_STATE.WAIT:
+                        break;
+                }
+
+                // 自動殺戮処理
                 if(this.transform.position.x > 20f){
                     Destroy(this.gameObject);
                 }
+
+                if(enemyHP <= 0f){
+                    Destroy(this.gameObject);
+                }
+
+                hp = enemyHP;
+
             }).AddTo(this);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Character")){
+            m_state = ENEMY_STATE.ATTACK;
+
+            m_charaMove = other.gameObject.GetComponent<CharacterMove>();
+            StartCoroutine(AttackCol(attackSpan));
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Character")){
+            m_state = ENEMY_STATE.MOVE;
+            StopCoroutine(AttackCol(attackSpan));
+        }
+    }
+
+    private IEnumerator AttackCol(float waittime){
+        while(true){
+            m_charaMove.charaHP = m_charaMove.charaHP - attackPower;
+
+            yield return new WaitForSeconds(waittime);
+        }
     }
 }
